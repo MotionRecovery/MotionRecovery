d22bb67ec1d02c628c914d7e26a5963af7591c72
djedje-cyber <58746678+djedje-cyber@users.noreply.github.com>
2023-11-20 17:26:52 +0100

    Begin user level feature

---
+++ initKinect/Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Kinect;
+using System.Windows;
+
+namespace motionRecovery
+{
+    internal class Test
+    {
+
+        public JointType[] WhichJoint { get; set; }
+        public Point getAngle { get; set; }
+
+        public Point getElbow {  get; set; }
+
+
+        public Point getHand { get; set; }
+
+        public Point getWrist { get; set;}
+       
+
+    }
+}
