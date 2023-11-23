b39581fabd667addcd673768f15325ca168da611
creuther01 <corentin.reuther@student.unamur.be>
2023-11-23 09:38:16 +0100

    ParseJointType improved in ExerciseReaderXML
    Allows you to read more parts of the body

--- initKinect/ExercisesReaderXML.cs
+++ initKinect/ExercisesReaderXML.cs
@@ -48,15 +48,48 @@ namespace motionRecovery
         {
             switch (jointTypeName)
             {
+                case "SpineBase":
+                    return JointType.SpineBase;
+                case "SpineMid":
+                    return JointType.SpineMid;
+                case "SpineShoulder":
+                    return JointType.SpineShoulder;
+                case "Neck":
+                    return JointType.Neck;
+                case "Head":
+                    return JointType.Head;
+                case "ShoulderLeft":
+                    return JointType.ShoulderLeft;
+                case "ElbowLeft":
+                    return JointType.ElbowLeft;
+                case "WristLeft":
+                    return JointType.WristLeft;
+                case "HandLeft":
+                    return JointType.HandLeft;
+                case "ShoulderRight":
+                    return JointType.ShoulderRight;
                 case "ElbowRight":
                     return JointType.ElbowRight;
                 case "WristRight":
                     return JointType.WristRight;
-                case "Head":
-                    return JointType.Head;
-                case "Neck":
-                    return JointType.Neck;
-
+                case "HandRight":
+                    return JointType.HandRight;
+                case "HipLeft":
+                    return JointType.HipLeft;
+                case "KneeLeft":
+                    return JointType.KneeLeft;
+                case "AnkleLeft":
+                    return JointType.AnkleLeft;
+                case "FootLeft":
+                    return JointType.FootLeft;
+                case "HipRight":
+                    return JointType.HipRight;
+                case "KneeRight":
+                    return JointType.KneeRight;
+                case "AnkleRight":
+                    return JointType.AnkleRight;
+                case "FootRight":
+                    return JointType.FootRight;
 
                 // Add more cases as needed for other joints
                 default:
