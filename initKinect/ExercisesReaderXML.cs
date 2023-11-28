e7bfc79f76ef7331e36d761eb8a90d4929f6f81c
creuther01 <corentin.reuther@student.unamur.be>
2023-11-28 16:34:42 +0100

    Adaptation of exercisesReaderXML to manage multi-position rules

--- initKinect/ExercisesReaderXML.cs
+++ initKinect/ExercisesReaderXML.cs
@@ -1,23 +1,19 @@
 ﻿using Microsoft.Kinect;
 using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Windows.Controls;
 using System.Xml;
-using System.Xml.Linq;
 
 namespace motionRecovery
 {
     // Class responsible for reading exercise data from an XML file
     internal class ExercisesReaderXML
     {
-
         private ParseJointType ParseJointType; // Used to parse a string to a type Joint
 
-        // Used to read exercise data from an XML file and return a list of positions
-        public List<Position> ReadExerciseFile(string filePath)
+        // Used to read exercise data from an XML file and return an ExerciseMultiPositon object
+        public ExerciseMultiPositon ReadExerciseFile(string filePath)
         {
-            List<Position> positionList = new List<Position>();
+            ExerciseMultiPositon exerciseMultiPositon = new ExerciseMultiPositon();
             ParseJointType parseJointType = new ParseJointType();
 
             try
@@ -25,20 +21,38 @@ namespace motionRecovery
                 XmlDocument xmlDoc = new XmlDocument();
                 xmlDoc.Load(filePath);
 
-                XmlNodeList positionNodes = xmlDoc.SelectNodes("//Position");
+                XmlNodeList exerciseNodes = xmlDoc.SelectNodes("//ExerciseMultiPositon");
 
-                foreach (XmlNode positionNode in positionNodes)
+                foreach (XmlNode exerciseNode in exerciseNodes)
                 {
-                    Position myPosition = new Position();
+                    exerciseMultiPositon.Name = exerciseNode.SelectSingleNode("Name").InnerText.Trim();
+                    exerciseMultiPositon.Description = exerciseNode.SelectSingleNode("Description").InnerText.Trim();
 
-                    myPosition.Joint1 = parseJointType.ParseToJoint(positionNode.SelectSingleNode("Membre1").InnerText.Trim());
-                    myPosition.Joint2 = parseJointType.ParseToJoint(positionNode.SelectSingleNode("Membre2").InnerText.Trim());
-                    myPosition.AngleMin = Convert.ToDouble(positionNode.SelectSingleNode("AngleMin").InnerText.Trim());
-                    myPosition.AngleMax = Convert.ToDouble(positionNode.SelectSingleNode("AngleMax").InnerText.Trim());
-                    myPosition.PositionTime = Convert.ToDouble(positionNode.SelectSingleNode("PositionTime").InnerText.Trim());
-                    myPosition.Description = positionNode.SelectSingleNode("Description").InnerText.Trim();
+                    XmlNodeList ruleNodes = exerciseNode.SelectNodes("Rules/ExerciseRule");
+                    foreach (XmlNode ruleNode in ruleNodes)
+                    {
+                        ExerciseRule exerciseRule = new ExerciseRule
+                        {
+                            PositionTime = Convert.ToDouble(ruleNode.SelectSingleNode("PositionTime").InnerText.Trim()),
+                            Description = ruleNode.SelectSingleNode("Description").InnerText.Trim()
+                        };
 
-                    positionList.Add(myPosition);
+                        XmlNodeList positionNodes = ruleNode.SelectNodes("Positions/SimplePosition");
+                        foreach (XmlNode positionNode in positionNodes)
+                        {
+                            SimplePosition simplePosition = new SimplePosition
+                            {
+                                Joint1 = parseJointType.ParseToJoint(positionNode.SelectSingleNode("Joint1").InnerText.Trim()),
+                                Joint2 = parseJointType.ParseToJoint(positionNode.SelectSingleNode("Joint2").InnerText.Trim()),
+                                AngleMin = Convert.ToDouble(positionNode.SelectSingleNode("AngleMin").InnerText.Trim()),
+                                AngleMax = Convert.ToDouble(positionNode.SelectSingleNode("AngleMax").InnerText.Trim())
+                            };
+
+                            exerciseRule.Positions.Add(simplePosition);
+                        }
+
+                        exerciseMultiPositon.Rules.Add(exerciseRule);
+                    }
                 }
             }
             catch (Exception ex)
@@ -46,65 +60,7 @@ namespace motionRecovery
                 Console.WriteLine($"An error occurred while reading the exercise file: {ex.Message}");
             }
 
-            return positionList;
+            return exerciseMultiPositon;
         }
-
-
-
-
-        public void WriteNewAttributes(string filePath, string attributes, string value)
-        {
-            XDocument doc = XDocument.Load(filePath);
-            XElement school = doc.Element("Position");
-            school.Add(new XElement("Intermidiate",
-                       new XElement(attributes, value)));
-            doc.Save(filePath);
-
-        }
-
-
-        public void WriteAttributes(string filepath,string attributes, string value)
-        {
-
-            try
-            {
-                // Load the XML file
-                XmlDocument xmlDoc = new XmlDocument();
-                xmlDoc.Load(filepath);
-
-                // Select the node where you want to add/modify the attribute
-                XmlNode node = xmlDoc.SelectSingleNode("/Position/"); // Update the XPath accordingly
-
-                // Check if the node exists
-                if (node != null)
-                {
-                    // Add or update the attribute
-                    XmlAttribute attribute = node.Attributes[attributes];
-                    if (attribute == null)
-                    {
-                        // If the attribute doesn't exist, create it
-                        attribute = xmlDoc.CreateAttribute(attributes);
-                        node.Attributes.Append(attribute);
-                    }
-
-                    // Set the attribute value
-                    attribute.Value = value;
-                }
-                else
-                {
-                    throw new InvalidOperationException("Node not found");
-                }
-
-                // Save the modified XML document
-                xmlDoc.Save(filepath);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-            }
-
-
-        }
-
     }
 }
