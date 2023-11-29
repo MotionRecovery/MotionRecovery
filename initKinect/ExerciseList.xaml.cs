9449ffc5f94a2b5f5404d236379d0941a4652e0c
creuther01 <corentin.reuther@student.unamur.be>
2023-11-29 22:20:46 +0100

    Improve GUI ExerciseList

--- initKinect/ExerciseList.xaml.cs
+++ initKinect/ExerciseList.xaml.cs
@@ -66,7 +66,7 @@ namespace motionRecovery
         }
 
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_CreateExercise(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new CreateExerciseXMLPage());
 
